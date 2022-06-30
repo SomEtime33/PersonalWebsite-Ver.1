4c524a8cd2a31156985ffcedf652309e76c785b8
SomEtime33 <hansomcho@gmail.com>
2022-06-30 16:31:23 +0900

    4

--- routes/aboutRoute.js
+++ routes/aboutRoute.js
@@ -1,25 +1,76 @@
 
 const router = require('express').Router();
-const {getAbout, addAbout, getAboutId, updateAbout, delAbout} = require('../controllers/aboutCtrl');
+const { getAbout, addAbout, getAboutId, updateAbout, delAbout } = require('../controllers/aboutCtrl');
+// const {getAbout, addAbout, getAboutId, updateAbout, delAbout} = require('../controllers/aboutCtrl');
+
+const aboutSchema = require('../models/aboutModel');
 
 
 
 // ..................about...................
 
 // get about user
-router.get('/about', getAbout)
+router.get('/about', async (req, res)=> {
+    const about = await aboutSchema.find();
+    
+    try {
+        res.json(about);
+    } catch (error) {
+        res.status(500).json({msg:'server problem'})
+    }
+
+
+    // //second way
+    // aboutSchema.find()
+    // .then(about=>res.json(about))
+    // .catch(err=>res.status(500).json('error: ${error}'))
+
+})
 
 // add about user
-router.post('/about', addAbout)
+router.post('/about', async (req, res)=> {
+    const {about} = req.body;
+
+    try {
+        const newAbout = new aboutSchema({
+
+            about
+    
+        })
+        await newAbout.save();
+        res.json(newAbout);
+    } catch (error) {
+        res.status(500).json({msg:'server problem'})
+    }
+
+    // //second way
+    // const newAbout = new aboutSchema({
+    //     about
+
+    // })
+
+    // newAbout.save()
+    // .then(about=>res.json('that areticle was sended')) 
+    // .catch(err=>res.status(500).json('error: ${error}'))
+
+
+
+})
 
 // get specific user by id
-router.get('/about/:id', getAboutId)
+router.get('/about/:id', (req, res)=> {
+    res.send('hello from get about by id')
+})
 
 // update speficific user by id
-router.put('/about/update/:id', updateAbout)
+router.put('/about/update/:id', (req, res)=> {
+    res.send('hello from update about')
+})
 
 // delete specific user by id
-router.delete('/about/:id', delAbout)
+router.delete('/about/:id', (req, res)=> {
+    res.send('hello from delete about')
+})
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -12,8 +12,24 @@ app.use(express.json());
 app.use(cors());
 
 
+//connect to mongodb
+const URI = process.env.MONGO_URL
+
+mongoose.connect(URI, (err)=> {
+    if (err) throw err;
+    else {
+        return console.log("mongoose is connected");
+    }
+})
+
+
+
+
+
 // routes
 app.use('/', require('./routes/aboutRoute'));
+
+
 
 
 
