11995546b92dd3ba0f33ab6ec6f403f78c1696d9
SomEtime33 <hansomcho@gmail.com>
2022-07-05 14:15:45 +0900

    5

--- controllers/aboutCtrl.js
+++ controllers/aboutCtrl.js
@@ -1,22 +1,77 @@
 
-exports.getAbout = (req, res)=> {
-    res.send('hello from get about')
+const aboutSchema = require("../models/aboutModel");
+
+
+//.....get about user.......
+exports.getAbout =  async (req, res)=> {
+    const about = await aboutSchema.find();
+    
+    try {
+        res.json(about);
+    } catch (error) {
+        res.status(500).json({msg:'server problem'})
+    }
+
 }
 
+//........add about user.......
+exports.addAbout = async (req, res)=> {
+    const {about} = req.body;
 
-exports.addAbout = (req, res)=> {
-    res.send('hello from add about')
+    try {
+        const newAbout = new aboutSchema({
+
+            about
+    
+        })
+        await newAbout.save();
+        res.json(newAbout);
+    } catch (error) {
+        res.status(500).json({msg:'server problem'})
+    }
+
+
+
 }
 
+//........get speficif about user by id.......
+exports.getAboutId =  async (req, res)=> {
 
-exports.getAboutId = (req, res)=> {
-    res.send('hello from get about by id')
+    try {
+        const about = await aboutSchema.findById(req.params.id);
+    res.json(about);
+
+    } catch (error) {
+        res.status(500).json({msg:'server problem'})
+    }
+
+
+}
+//.........update about speficif user by id........
+exports.updateAbout = async (req, res)=> {
+    const {about} = req.body;
+   
+    try {
+        const newAbout = await aboutSchema.findByIdAndUpdate(req.params.id, {
+
+            about
+    
+        });
+        let results = await newAbout.save();
+        await results;
+        res.json({msg: "Item updated"}) 
+    } catch (error) {
+        res.status(500).json({msg:'server problem'})
+    }
+
 }
 
-exports.updateAbout = (req, res)=> {
-    res.send('hello from update about')
-}
+//........delete speficif about user by id........
+exports.delAbout = async (req, res)=> {
+    const about = await aboutSchema.findByIdAndDelete(req.params.id)
 
-exports.delAbout = (req, res)=> {
-    res.send('hello from delete about')
+    about;
+
+    res.json({msg: "item deleted"})
+
 }
