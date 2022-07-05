87d693bd05d8511ab7484e3745161d80d31a4d9f
SomEtime33 <hansomcho@gmail.com>
2022-07-05 14:16:13 +0900

    5

--- routes/aboutRoute.js
+++ routes/aboutRoute.js
@@ -1,76 +1,31 @@
 
 const router = require('express').Router();
-const { getAbout, addAbout, getAboutId, updateAbout, delAbout } = require('../controllers/aboutCtrl');
-// const {getAbout, addAbout, getAboutId, updateAbout, delAbout} = require('../controllers/aboutCtrl');
+const {getAbout, addAbout, getAboutId, updateAbout, delAbout} = require('../controllers/aboutCtrl');
 
-const aboutSchema = require('../models/aboutModel');
+// const aboutSchema = require('../models/aboutModel');
 
 
 
 // ..................about...................
 
 // get about user
-router.get('/about', async (req, res)=> {
-    const about = await aboutSchema.find();
-    
-    try {
-        res.json(about);
-    } catch (error) {
-        res.status(500).json({msg:'server problem'})
-    }
+router.get('/about', getAbout)
 
-
-    // //second way
-    // aboutSchema.find()
-    // .then(about=>res.json(about))
-    // .catch(err=>res.status(500).json('error: ${error}'))
-
-})
 
 // add about user
-router.post('/about', async (req, res)=> {
-    const {about} = req.body;
+router.post('/about', addAbout)
 
-    try {
-        const newAbout = new aboutSchema({
-
-            about
-    
-        })
-        await newAbout.save();
-        res.json(newAbout);
-    } catch (error) {
-        res.status(500).json({msg:'server problem'})
-    }
-
-    // //second way
-    // const newAbout = new aboutSchema({
-    //     about
-
-    // })
-
-    // newAbout.save()
-    // .then(about=>res.json('that areticle was sended')) 
-    // .catch(err=>res.status(500).json('error: ${error}'))
-
-
-
-})
 
 // get specific user by id
-router.get('/about/:id', (req, res)=> {
-    res.send('hello from get about by id')
-})
+router.get('/about/:id',getAboutId)
+
 
 // update speficific user by id
-router.put('/about/update/:id', (req, res)=> {
-    res.send('hello from update about')
-})
+router.put('/about/update/:id', updateAbout)
+
 
 // delete specific user by id
-router.delete('/about/:id', (req, res)=> {
-    res.send('hello from delete about')
-})
+router.delete('/about/:id', delAbout)
 
 
 module.exports = router;
